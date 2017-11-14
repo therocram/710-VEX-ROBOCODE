4a0b16b91fb6650ab6ede41e278af109a36d9351
Therocram <therocram@gmail.com>
2017-11-14 15:50:29 -0700

    Fixed Some severe problems

--- Controller Code.c
+++ Controller Code.c
@@ -8,80 +8,80 @@
 
 /* Controler Code
 
-	This is the code provided on Canvas for POE
+This is the code provided on Canvas for POE
 
-	Controlls 2 motors
+Controlls 2 motors
 ------------------------------------------------------------------------------------------------------------------------
-	Instructions on connecting Controller to Cortex:
+Instructions on connecting Controller to Cortex:
 
-		1. Connect 7.2V battery to VEX Cortex, then install batteries into VEX controller.
+1. Connect 7.2V battery to VEX Cortex, then install batteries into VEX controller.
 
-		2. DO NOT turn on the Cortex or the controller. Connect one end of a USB A-to-A cable into
-		the Cortex. Then connect the other end into the VEX controller.
+2. DO NOT turn on the Cortex or the controller. Connect one end of a USB A-to-A cable into
+the Cortex. Then connect the other end into the VEX controller.
 
-		3. Turn the Cortex on. Wait 15 seconds for the Robot and VEXNet LED's on the cortex to turn green.
-		Turn the Cortex off and unplug the USB cable from the controller and the cortex.
+3. Turn the Cortex on. Wait 15 seconds for the Robot and VEXNet LED's on the cortex to turn green.
+Turn the Cortex off and unplug the USB cable from the controller and the cortex.
 
-		4. Insert VEXnet USB Keys into the VEXnet Remote Control and Cortex. It doesn't matter which key
-		you put in what. Turn the Cortex on and the controller on and wait 15 seconds for the LED's on both to turn green.
+4. Insert VEXnet USB Keys into the VEXnet Remote Control and Cortex. It doesn't matter which key
+you put in what. Turn the Cortex on and the controller on and wait 15 seconds for the LED's on both to turn green.
 
-		5. Enjoy!
+5. Enjoy!
 ------------------------------------------------------------------------------------------------------------------------
 
 
 */
 
 void ControllerCode(void)
+{
+	while(1 == 1)
 	{
-		while(1 == 1)
-		  {
-		    int armSpeedUp = 127;
-		    int armSpeedDown = -armSpeedUp;
-		    int clawSpeed = 127;
-		    int clawSpeed2 = -clawSpeed;
+		int armSpeedUp = 127;
+		int armSpeedDown = -armSpeedUp;
+		int clawSpeed = 127;
+		int clawSpeed2 = -clawSpeed;
 
-		  	motor[leftMotor]  = (vexRT[Ch2] + vexRT[Ch1])/2;  // (y + x)/2
-		    motor[rightMotor] = (vexRT[Ch2] - vexRT[Ch1])/2;  // (y - x)/2
+		motor[leftMotor]  = (vexRT[Ch2] + vexRT[Ch1])/2;  // (y + x)/2
+		motor[rightMotor] = (vexRT[Ch2] - vexRT[Ch1])/2;  // (y - x)/2
 
-		    motor[bottomLeft] = (-vexRT[Ch3] - vexRT[Ch4]);  // (-x - y)
-		    motor[bottomRight] = (-vexRT [Ch3] + vexRT[Ch4]); // (-x + y)
+		motor[bottomLeft] = (-vexRT[Ch3] - vexRT[Ch4]);  // (-x - y)
+		motor[bottomRight] = (-vexRT [Ch3] + vexRT[Ch4]); // (-x + y)
 
 
-		    if(vexRT[Btn7U] == 1)
-    		{
-      		motor[clawArm] = armSpeedUp;
-   			}
-
-   			else if(vexRT[Btn7D] == 1)
-    		{
-      		motor[clawArm] = armSpeedDown;
-    		}
-
-    		else
-    		{
-      		motor[clawArm] = 0;
-    		}
-
-
-    while(1 == 1)
-    {
-
-    		if(vexRT[Btn7L] == 1)
-		    {
-		      motor[clawMotor] = clawSpeed;
-		    }
-		    else if(vexRT[Btn7R] == 1)
-		    {
-		      motor[clawMotor] = clawSpeed2;
-		    }
-		    else
-		    {
-		      motor[clawMotor] = 0;
-		    }
+		if(vexRT[Btn7U] == 1)			// Button 7U makes the arm go up
+		{
+			motor[clawArm] = armSpeedUp;
 		}
 
-    	}
+		else if(vexRT[Btn7D] == 1)  //Buton 7D makes the arm go down
+		{
+			motor[clawArm] = armSpeedDown;
+		}
+
+		else
+		{
+			motor[clawArm] = 0;
+		}
+
+
+
+		if(vexRT[Btn7L] == 1)  //Button 7L makes the claw open
+		{
+			motor[clawMotor] = clawSpeed;
+		}
+
+		else if(vexRT[Btn8R] == 1)	// Button 7R makes the claw close
+		{
+			motor[clawMotor] = clawSpeed2;
+		}
+
+		else
+		{
+			motor[clawMotor] = 0;
+		}
+
+
 	}
+}
 
 
 task main()
