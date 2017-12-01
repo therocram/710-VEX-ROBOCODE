3c38a697abf5602b17530b60501265750bcf3339
Therocram <therocram@gmail.com>
2017-12-01 12:36:57 -0700

    Still have to modify the autonomous

--- Competition Code.c
+++ Competition Code.c
@@ -7,6 +7,8 @@
 #pragma config(Motor,  port8,           bottomLeft2,   tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port9,           bottomRight2,  tmotorVex393_MC29, openLoop)
 #pragma config(Sensor, dgtl1,  clawSensor,     sensorSONAR_cm)
+#pragma config(Sensor, dgtl3,  frontSensor,    sensorSONAR_cm)
+#pragma config(Sensor, dgtl5,  backSensor,     sensorSONAR_cm)
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -58,89 +60,12 @@ void pre_auton()
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-static void FullSpeedAhead(void) //This makes it go forward really fast
-{
-	startMotor(bottomLeft, 127);
-	startMotor(bottomRight, 127);
-}
-static void WhoaNellie(void) //This makes it do a stop and a turn around
-{
-	startMotor(bottomLeft, -127);
-	startMotor(bottomRight, -127);
-	wait(2);
-	startMotor(bottomLeft, 127);
-	startMotor(bottomRight, -127);
-	wait(2);
-	stopMotor(bottomLeft);
-	stopMotor(bottomRight);
-}
-static void StopNow(void) //This just makes it stop
-{
-	stopMotor(bottomLeft);
-	stopMotor(bottomRight);
-}
 
-
-
-static int CCsensorDistance = 3; //Claw Cone Sensor Distance for detection is 3cm
-static int armSpeedUp = 127;
-static int armSpeedDown = -armSpeedUp;
-static int clawSpeed = 127;
-static int clawSpeed2 = -clawSpeed;
-static bool ConeLoaded = false;  //Starts with no cone loaded
-
-
-
-void RetractWhenLoaded(void)
-{
-	if(SensorValue[clawSensor] == CCsensorDistance) 	//If an object is detected within sensor distance then...
-	{
-		startMotor(clawMotor, clawSpeed2); 			//Close Claw
-		startMotor(clawArm, armSpeedUp);			//Raise Arm up
-		wait(0.5);
-		stopMotor(clawMotor); 		//Stop both
-		stopMotor(clawArm);
-		ConeLoaded = true; 			//Tell program that cone is loaded
-	}
-	else
-	{
-		motor[clawArm] = 0;
-		motor[clawMotor] = 0;
-	}
-}
-
-void NoConeLoaded(void)
-{
-	if(ConeLoaded == false)		//If no cone is loaded then...
-	{
-		startMotor(clawMotor, clawSpeed);		//Open claw
-		startMotor(clawArm, armSpeedDown);	//Lower arm
-		wait(1);
-		stopMotor(clawMotor);
-		stopMotor(clawArm);
-	}
-
-}
 
 
 task autonomous()
 {
-  while(1 == 1)
-	{
-		RetractWhenLoaded();
 
-		NoConeLoaded();
-
-		if(ConeLoaded == true) //When a cone is loaded the robot goes full speed ahead
-		{
-			FullSpeedAhead();
-		}
-		if(ConeLoaded == false)
-		{
-			WhoaNellie();
-			StopNow();
-		}
-	}
 }
 
 /*---------------------------------------------------------------------------*/
